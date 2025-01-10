4141ac26eb32545e5030450b95945fe34a0e68a8
Marike Vossbeck <marike_vossbeck@posteo.de>
2025-01-10 11:31:40 +0100

    init project

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\jupib\Flutter\flutter_windows_3.24.5-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\jupib\StudioProjects\CarSharing"
+export "FLUTTER_ROOT=/home/marike/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/marike/StudioProjects/carpooling_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
